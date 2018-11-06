608a9d325f9a8427aa7981e776ce4d2d792c941e
rohan mahajan <rohanm@rohans-MacBook-Air.local>
2018-11-05 16:26:57 -0800

    refactored into class format

--- linkedin_scraper.py
+++ linkedin_scraper.py
@@ -1,5 +1,18 @@
 import time
 from selenium import webdriver
 
-driver = webdriver.Chrome('./chromedriver')  # Optional argument, if not specified will search path.
-driver.get('https://www.linkedin.com/feed/');
+
+class LinkedinScraper:
+    def __init__(self):
+        self.driver = webdriver.Chrome('./chromedriver')
+
+    def login(self):
+        self.driver.get('https://www.linkedin.com/feed/')
+
+    def scrape(self):
+        self.login()
+
+
+if __name__ == "__main__":
+    linkedin_scraper = LinkedinScraper()
+    linkedin_scraper.scrape()
