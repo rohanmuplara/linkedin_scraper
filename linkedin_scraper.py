b05454cf0c6d3f3bc0699819d739febc382b56cf
rohan mahajan <rohanm@rohans-MacBook-Air.local>
2018-11-05 16:37:16 -0800

    getting login finished

--- linkedin_scraper.py
+++ linkedin_scraper.py
@@ -7,7 +7,13 @@ class LinkedinScraper:
         self.driver = webdriver.Chrome('./chromedriver')
 
     def login(self):
-        self.driver.get('https://www.linkedin.com/feed/')
+        self.driver.get('https://www.linkedin.com/')
+	login_username = self.driver.find_element_by_id('login-email')
+	login_password = self.driver.find_element_by_id('login-password')
+        login_username.send_keys("rohanmahajan1993@gmail.com")
+        login_password.send_keys("warriors93")
+	login_submit = self.driver.find_element_by_id('login-submit')
+        login_submit.click()
 
     def scrape(self):
         self.login()
