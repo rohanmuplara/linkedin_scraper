e412c008075053a4c13c2c9068edc6f6a4c133ec
Rohan Mahajan <rohanmahajan1993@gmail.com>
2018-11-06 09:55:06 -0800

    test commit

--- linkedin_scraper.py
+++ linkedin_scraper.py
@@ -19,15 +19,38 @@ class LinkedinScraper:
         login_submit = self.driver.find_element_by_id('login-submit')
         login_submit.click()
 
-    def search(self):
+    def generate_connection_urls(self):
         search_bar = self.driver.find_element_by_xpath(
             "//input[@role='combobox']")
         search_bar.send_keys(self.company_name)
         search_bar.send_keys(Keys.RETURN)
+        connections = set()
+        while True:
+            time.sleep(10)
+            self.driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight)")
+            time.sleep(3)
+            self.driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight / 2.0)")
+            time.sleep(5)
+            self.driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight)")
+            search_results = self.driver.find_elements_by_xpath(
+                "//a[@data-control-name='search_srp_result']")
+            for search_result in search_results:
+                connection_url = search_result.get_attribute("href")
+                connections.add(connection_url)
+                print connection_url
+            next_button = self.driver.find_element_by_xpath(
+                "//button[contains(@class, 'artdeco-pagination__button--next')]"
+            )
+            next_button.click()
+        print connections
+        return connections
 
     def scrape(self):
         self.login()
-        self.search()
+        self.generate_connection_urls()
 
 
 if __name__ == "__main__":
