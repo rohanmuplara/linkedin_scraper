1c869ccad0177b2997328c933dcb7ba237b52105
Rohan Mahajan <rohanmahajan1993@gmail.com>
2018-11-06 15:22:47 -0800

    Adding the logging

--- linkedin_scraper.py
+++ linkedin_scraper.py
@@ -1,5 +1,7 @@
+import datetime
 import sys
 import time
+import logging as custom_logging
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -9,6 +11,14 @@ class LinkedinScraper:
     def __init__(self, company_name):
         self.driver = webdriver.Chrome('./chromedriver')
         self.company_name = company_name
+        current_time = datetime.datetime.utcnow().strftime(
+            "%I:%M%p on %B %d, %Y")
+        log_file_name = company_name + " " + current_time
+        custom_logging.basicConfig(
+            level=custom_logging.INFO,
+            filename=log_file_name,
+            filemode='w',
+            format='%(levelname)s - %(message)s')
 
     def login(self):
         self.driver.get('https://www.linkedin.com/')
@@ -40,20 +50,43 @@ class LinkedinScraper:
             for search_result in search_results:
                 connection_url = search_result.get_attribute("href")
                 connections.add(connection_url)
-                print connection_url
             try:
                 next_button = self.driver.find_element_by_xpath(
                     "//button[contains(@class, 'artdeco-pagination__button--next')]"
                 )
                 next_button.click()
+                break
             except:
                 break
         print connections
         return connections
 
+    def add_connections(self, connection_urls):
+        for connection_url in connection_urls:
+            self.driver.get(connection_url)
+            try:
+                connect_button = self.driver.find_element_by_xpath(
+                    "//button[contains(@class, 'pv-s-profile-actions--connect')]"
+                )
+                connect_button.click()
+                import pdb
+                pdb.set_trace()
+            except:
+                try:
+                    overflow_button = self.driver.find_element_by_xpath(
+                        "//button[contains(@class, 'pv-s-profile-actions__overflow-toggle')]"
+                    )
+                    connect_button = self.driver.find_element_by_xpath(
+                        "//button[contains(@class, 'pv-s-profile-actions--connect')]"
+                    )
+                    connect_button.click()
+                except:
+                    a = 5
+
     def scrape(self):
         self.login()
         connection_urls = self.generate_connection_urls()
+        self.add_connections(connection_urls)
 
 
 if __name__ == "__main__":
